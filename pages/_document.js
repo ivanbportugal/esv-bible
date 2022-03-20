77f0ebbd213171d1e0d7fd2fe8a090d8c1c50c4b
Ivan Portugal <ivanbportugal@gmail.com>
2022-03-20 16:44:13 -0500

    manifest updates

--- pages/_document.js
+++ pages/_document.js
@@ -1,12 +1,17 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import { prefix } from '../lib/prefix.js';
 
 class MyDocument extends Document {
   render() {
+    const favicon = `${prefix}/favicon.ico`;
+    const manifest = `${prefix}/manifest.json`;
+    const apple = `${prefix}/icon.png`;
     return (
       <Html>
         <Head>
-          <link rel="manifest" href="/manifest.json" />
-          <link rel="apple-touch-icon" href="/icon.png"></link>
+          <link rel="icon" href={favicon} />
+          <link rel="manifest" href={manifest} />
+          <link rel="apple-touch-icon" href={apple}></link>
           <meta name="theme-color" content="#fff" />
         </Head>
         <body>
