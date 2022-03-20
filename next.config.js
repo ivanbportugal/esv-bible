1d42960fb969115a5338bd673cf87776d921740b
Ivan Portugal <ivanbportugal@gmail.com>
2022-03-20 16:25:00 -0500

    attempt github pages

--- next.config.js
+++ next.config.js
@@ -4,6 +4,8 @@ const withPWA = require('next-pwa')
 const nextConfig = 
   withPWA({
     reactStrictMode: true,
+    basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+    assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
     pwa: {
       dest: "public",
       register: true,
